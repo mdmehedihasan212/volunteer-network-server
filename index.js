3f90e086f102b0d86f183b7bbb93983ad6347a85
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-26 14:27:41 +0600

    add get volunteer api

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@volunteer-network.54muj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+        await client.connect();
+        const volunteerCollection = client.db('VolunteerNetwork').collection('volunteer');
+
+        app.get('/volunteer', async (req, res) => {
+            const query = {};
+            const cursor = volunteerCollection.find(query);
+            const volunteers = await cursor.toArray();
+            res.send(volunteers);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
 
 app.get('/', (req, res) => {
     res.send('Volunteer Network Server')
